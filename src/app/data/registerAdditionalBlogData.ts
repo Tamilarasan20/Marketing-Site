8c34c0c5501c10dd59a8b2f3d2d024f1cdfeaecb
Tamilarasan karuppusamy <67625310+Tamilarasan20@users.noreply.github.com>
2026-06-01 02:55:25 +0530

    Register Sintra-inspired Loraloop blog posts

--- src/app/data/registerAdditionalBlogData.ts
+++ src/app/data/registerAdditionalBlogData.ts
@@ -1,7 +1,8 @@
 import { blogPosts } from './blogData';
 import { additionalBlogPosts } from './additionalBlogData';
+import { sintraInspiredBlogPosts } from './sintraInspiredBlogData';
 
-for (const post of additionalBlogPosts) {
+for (const post of [...additionalBlogPosts, ...sintraInspiredBlogPosts]) {
   const alreadyExists = blogPosts.some((existingPost) => existingPost.id === post.id || existingPost.slug === post.slug);
 
   if (!alreadyExists) {
